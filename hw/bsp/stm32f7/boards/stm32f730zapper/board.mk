72312805c5eb27ff12a5b6c87727d73d77241d64
Adam Malinowski <adam.malinowski@canonical.com>
2022-05-12 20:18:42 +0200

    Add zapper main board definition for F730 uc

---
+++ hw/bsp/stm32f7/boards/stm32f730zapper/board.mk
@@ -0,0 +1,15 @@
+PORT ?= 0
+SPEED ?= high
+
+CFLAGS += \
+  -DSTM32F730xx \
+  -DHSE_VALUE=8000000 \
+
+LD_FILE = $(BOARD_PATH)/STM32F7xx_FLASH.ld
+SRC_S += $(ST_CMSIS)/Source/Templates/gcc/startup_stm32f730xx.s
+
+# flash target using on-board stlink
+flash: flash-stlink
+
+# For flash-jlink target
+JLINK_DEVICE = stm32f723ie
